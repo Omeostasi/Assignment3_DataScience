b05307042d0c7f21cd503d19ede37a72fd91b7db
Giovanni Vincenzo Cavallo <202402143@post.au.dk>
2024-11-04 15:05:32 +0100

    Added some explanations about commitments. I am actually learning by doing it

--- How to commit.R
+++ How to commit.R
@@ -5,3 +5,19 @@
 #Commit to main
 
 #Git status shows you what you still need to commit and where your HEAD is. (head is the pointer that you use to see on which branch are you working on)
+
+"
+# git add [file]
+is it useful to add to a pending stage the file you want to commit. You can easily do it from the git tab on Rstudio. You can choose
+which file stage and then push.
+
+# git commit -m '[message]'
+
+code to commit your pending changes in your version history. They don't go into remote (therefore they are not shared).
+It basically affects only your local repostiory.
+
+# git push 
+
+pushes the commits to remote which is basically the shared repository
+
+"
