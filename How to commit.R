25d6f5cecd263fa8abd741a383848a9d5a83269b
Giovanni Vincenzo Cavallo <202402143@post.au.dk>
2024-11-04 14:18:51 +0100

    Added explanation about git status

--- How to commit.R
+++ How to commit.R
@@ -3,3 +3,5 @@
 #Basically add in the folder, than commit using git tab on R studio. Write a comment
 #Remember to save your file
 #Commit to main
+
+#Git status shows you what you still need to commit and where your HEAD is. (head is the pointer that you use to see on which branch are you working on)
